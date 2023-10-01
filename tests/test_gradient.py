79072ea918f030cc4ed39c492cbe0cf5c5e60dcb
Brandon Lee <leebr48@gmail.com>
2023-10-01 00:51:31 -0400

    tests: add a test for gradient

--- tests/test_gradient.py
+++ tests/test_gradient.py
@@ -1,1 +1,11 @@
 # Last names: L, M
+
+from minecraft_gen import gradient
+import numpy as np
+
+def test_gradient():
+    dummy_im_smooth = np.random.default_rng().random(100)[np.newaxis]
+    grad = gradient(dummy_im_smooth)
+
+    assert not np.isnan(grad).any()
+    assert not np.isinf(grad).any()
