a3e06767a31ef693a02040a57869578cfb51c6a3
miapeeples <138705330+miapeeples@users.noreply.github.com>
2023-10-02 12:50:54 -0400

    Update test_histeq.py

--- tests/test_histeq.py
+++ tests/test_histeq.py
@@ -5,6 +5,17 @@ import numpy as np
 
 def test_histeq():
   img_test = np.ones((3,3))
-  hist_test = histeq(img_test, 1)
+  alpha = 1
+  
+  hist_test = histeq(img_test, alpha)
+  
+  #copied code from histeq to ensure output is consistent with previous version
+  img_cdf, bin_centers = exposure.cumulative_distribution(img_test)
+  img_eq = np.interp(img_test, bin_centers, img_cdf)
+  img_eq = np.interp(img_eq, (0, 1), (-1, 1))
+  hist_compare = alpha * img_eq + (1 - alpha) * img
+  
+  assert hist_test.shape == (3, 3)
   assert not np.isnan(hist_test).any()
   assert not np.isinf(hist_test).any()
+  assert hist_test == hist_compare
