b447e8746f3e969d973af7ff4be89bc61023d4d0
miapeeples <138705330+miapeeples@users.noreply.github.com>
2023-10-02 12:29:25 -0400

    test added: test_histeq.py

--- tests/test_histeq.py
+++ tests/test_histeq.py
@@ -1,1 +1,10 @@
 # Last names: P
+
+from minecraft_gen import histeq
+import numpy as np
+
+def test_histeq():
+  img_test = np.ones((3,3))
+  hist_test = histeq(img_test, 1)
+  assert not np.isnan(hist_test).any()
+  assert not np.isinf(hist_test).any()
