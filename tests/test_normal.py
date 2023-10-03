3429b32f5835a0203008413e3bd27357f47ce713
Maxwell-Rosen <mrquell@gmail.com>
2023-10-03 17:54:08 -0400

    Added test_get_normal_light

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,1 +1,14 @@
 # Last names: Q, R
+from minecraft_gen import compute_normal_map, get_normal_light, get_normal_map
+import numpy as np
+
+def test_get_normal_light():
+    height_map = np.array([[0.0, 0.5], [0.8, 1.0]])
+    expected_result = np.array([ [0.65027821, 0.65267539], [0.60773718, 0.61224008]])
+
+    result = get_normal_light(height_map)
+    
+    assert result.shape == expected_result.shape
+    tolerance = 1e-6
+    assert np.allclose(result, expected_result, rtol=tolerance, atol=tolerance)
+
