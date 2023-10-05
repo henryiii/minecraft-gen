cb75fb74c3d5478a0afc7131ec256b16b2092e1a
Maxwell Rosen <48170600+Maxwell-Rosen@users.noreply.github.com>
2023-10-05 10:17:39 -0400

    tests: added test_get_normal_light (#7)
    * Added test_get_normal_light
    
    * Update tests/test_normal.py
    
    Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>
    
    * Update tests/test_normal.py
    
    Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>
    
    ---------
    
    Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,1 +1,15 @@
 # Last names: Q, R
+from minecraft_gen import compute_normal_map, get_normal_light, get_normal_map
+import numpy as np
+import pytest
+
+def test_get_normal_light():
+    height_map = np.array([[0.0, 0.5], [0.8, 1.0]])
+    expected_result = np.array([ [0.65027821, 0.65267539], [0.60773718, 0.61224008]])
+
+    result = get_normal_light(height_map)
+    
+    assert result.shape == expected_result.shape
+    tolerance = 1e-6
+    assert result == pytest.approx(expected_result, rel=tolerance, abs=tolerance)
+
