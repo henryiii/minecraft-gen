298ac16ad61ab1132170d6d8dc463870ccb09555
Maxwell Rosen <48170600+Maxwell-Rosen@users.noreply.github.com>
2023-10-05 10:15:27 -0400

    Update tests/test_normal.py
    Co-authored-by: Henry Schreiner <HenrySchreinerIII@gmail.com>

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,6 +1,7 @@
 # Last names: Q, R
 from minecraft_gen import compute_normal_map, get_normal_light, get_normal_map
 import numpy as np
+import pytest
 
 def test_get_normal_light():
     height_map = np.array([[0.0, 0.5], [0.8, 1.0]])
