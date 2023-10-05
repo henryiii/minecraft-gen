25f8c78e7ef4e9fa0a80c115a3e52088540a2dbb
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-10-05 10:22:36 -0400

    chore: add pre-commit (#8)
    * chore: add requires-python
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * chore: add pre-commit
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * style: run pre-commit
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_histeq.py
+++ tests/test_histeq.py
@@ -7,15 +7,15 @@ from skimage import exposure
 def test_histeq():
   img_test = np.ones((3,3))
   alpha = 1
-  
+
   hist_test = histeq(img_test, alpha)
-  
+
   #copied code from histeq to ensure output is consistent with previous version
   img_cdf, bin_centers = exposure.cumulative_distribution(img_test)
   img_eq = np.interp(img_test, bin_centers, img_cdf)
   img_eq = np.interp(img_eq, (0, 1), (-1, 1))
   hist_compare = alpha * img_eq + (1 - alpha) * img_test
-  
+
   assert hist_test.shape == (3, 3)
   assert not np.isnan(hist_test).any()
   assert not np.isinf(hist_test).any()
