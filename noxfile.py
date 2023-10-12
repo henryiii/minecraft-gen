375c35a6141b91f5a21b7a18e6bc7da6fa476232
Henry Schreiner <henryschreineriii@gmail.com>
2023-10-12 11:36:49 -0400

    feat: src folder and noxfile
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ noxfile.py
@@ -0,0 +1,33 @@
+import nox
+
+nox.options.sessions = ["lint", "tests"]
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run(
+        "pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs
+    )
+
+
+@nox.session
+def tests(session):
+    """
+    Run the test suite.
+    """
+    session.install("-e.[test]")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """
+    Build an SDist and wheel.
+    """
+
+    session.install("build")
+    session.run("python", "-m", "build")
